ae0e8afdfa8ada1f52182b76ba05842a7c050419
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-03-06 11:51:29 +0100

    Add exercise

--- src/universitat.py
+++ src/universitat.py
@@ -5,19 +5,29 @@ doc = etree.parse("xml/universitat.xml")
 
 
 print("\nNombre de la universidad:")
-
+print()
 for nombre in doc.xpath("/universidad/nombre/text()"):
     print(nombre + "\n")
 
 
 print("País de la universidad:")
-
+print()
 for pais in doc.xpath("/universidad/pais/text()"):
     print(pais + "\n")
 
 
 print("Nombres de las carreras:")
-
+print()
 for nombre_carrera in doc.xpath("//carrera/nombre/text()"):
     print(nombre_carrera)
+print()
+
+
+print("Años de plan de estudio de las carreras:")
+print()
+for plan_carrera in doc.xpath("//carrera/plan/text()"):
+    print(plan_carrera)
+
+
+
     
