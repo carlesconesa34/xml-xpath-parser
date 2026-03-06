1e2b1542b04c382af592d8b74240355224e6d601
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-03-06 11:48:19 +0100

    Add exercise

--- src/universitat.py
+++ src/universitat.py
@@ -8,3 +8,16 @@ print("\nNombre de la universidad:")
 
 for nombre in doc.xpath("/universidad/nombre/text()"):
     print(nombre + "\n")
+
+
+print("País de la universidad:")
+
+for pais in doc.xpath("/universidad/pais/text()"):
+    print(pais + "\n")
+
+
+print("Nombres de las carreras:")
+
+for nombre_carrera in doc.xpath("//carrera/nombre/text()"):
+    print(nombre_carrera)
+    
